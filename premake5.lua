8dca021d9bf22e059ecb124b2bdce573ff594af7
Marius Alexandru Nastasa <mariusalexandrunastasa@gmail.com>
2024-05-06 20:34:54 +0300

    update premake file

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+        
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/*.c"
+    }
+        
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+            
+        files
+        {
+            "src/*.c",
+        }
+    
+        defines 
+        { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"

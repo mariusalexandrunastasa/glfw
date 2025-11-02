92c25615e55accdb2d55f6611db464808a3ce3cc
Marius Alexandru Nastasa <mariusalexandrunastasa@gmail.com>
2025-11-02 19:49:22 +0200

    Update premake file for DuelEngine

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,28 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
-        
+
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/obj/" .. outputdir .. "/%{prj.name}")
-    
+
     files
     {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/*.c"
     }
-        
+
     filter "system:windows"
         systemversion "latest"
         staticruntime "On"
-            
+
         files
         {
             "src/*.c",
         }
-    
-        defines 
-        { 
+
+        defines
+        {
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
         }
